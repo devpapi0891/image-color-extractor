974531fd2e10b8d667cab83b9922888a4cf19b5c
devpapi0891 <rafael50891.dev@gmail.com>
2023-09-03 05:51:45 +0800

    added new files and deleted old files. README modification

--- components/pageLinks.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("PAGE_LINKS_ARRAY", 
-[
-    // 'Home' => './',
-    // 'About' => 'about',
-    // 'Contact' => 'contact',
-    // 'Donate' => 'donation',
-    // 'Merch' => 'merch'
-] );
